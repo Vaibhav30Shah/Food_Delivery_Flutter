2b1e391b9e5192b522db1080e88afb743834ffb3
Vaibhav30Shah <vaibhavshah.20.cs@iite.indusuni.ac.in>
2024-03-31 12:06:09 +0530

    authentication

---
+++ lib/authentication/authentication_helper.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthenticationHelper {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  get user => _auth.currentUser;
+
+  //SIGN UP METHOD
+  Future signUp({required String email, required String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //SIGN IN METHOD
+  Future signIn({required String email,required String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future<void> storeUserData(String token, Map<String, dynamic> userData) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+    await prefs.setString('userData', jsonEncode(userData));
+  }
+
+  Future<void> logout() async {
+    // Remove the stored user ID from SharedPreferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('userId');
+
+    // Sign out from Firebase Authentication
+    await _auth.signOut();
+  }
+
+  //SIGN OUT METHOD
+  Future signOut() async {
+    await _auth.signOut();
+
+    print('signout');
+  }
+}
